693b88df2bc570453c930b3e4e619ccc6a60bc2b
Raphael Macayan <111346262+manzs123@users.noreply.github.com>
2023-05-27 12:21:36 +0800

    Update v1.2.2

---
+++ jquery.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    
+    $('#hambIcon').click(function(){
+        $('#mobMenu').css("transform","translateX(0vw)");
+    });
+
+    $('#closeIcon').click(function(){
+        $('#mobMenu').css("transform","translateX(100vw)");
+    });
+
+
+
+})
